ba874192228fc9f20dd18993a6bf544c944e729e
FranciscoLopes01 <cursoupskills@gmail.com>
2022-08-30 11:44:38 +0100

    toques

--- Hugo-Quatro_em_Linha/4emlinha.js
+++ Hugo-Quatro_em_Linha/4emlinha.js
@@ -1,22 +1,22 @@
-let ready = $(document).ready(function() {
-    let player = 1;
+let ready = $(document).ready(function(){
+    let player= 1;
     let winner = 0;
-    var colors = {};
-    colors[-1] = "yellow";
-    colors[1] = "red";
+    var colors= {};
+    colors[-1]= "yellow";
+    colors[1]="red";
     let count = 0;
 
-    $(".Espaço").each(function () {
-        $(this).attr("id", count);
-        $(this).attr("data-player", 0);
+    $(".Espaço").each(function (){
+        $(this).attr("id",count);
+        $(this).attr("data-player",0);
         count++;
 
-        $(this).click(function () {
-            if (isvalid($(this).attr("id"))) {
-                $(this).css("background-color", colors[player]);
-                $(this).attr("data-player", player);
-                if (checkWin(player)) {
-                    alert(colors[player] + " Ganhou!")
+        $(this).click(function (){
+            if(isvalid($(this).attr("id"))){
+                $(this).css("background-color",colors[player]);
+                $(this).attr("data-player",player);
+                if(checkWin(player)){
+                    alert(colors[player]+ " Ganhou!")
                     winner = player;
                 }
 
@@ -26,59 +26,59 @@ let ready = $(document).ready(function() {
         });
 
     });
-
-    function isvalid(n) {
-        var id = parseInt(n);
-        if (winner !== 0) {
+    function isvalid(n){
+        var id= parseInt(n);
+        if(winner !== 0){
             return false;
         }
-        if ($("#" + id).attr("data-player") === "0") {
-            if (id >= 35) {
+        if($("#"+id).attr("data-player")  === "0"){
+            if(id >= 35) {
                 return true;
             }
-            if ($("#" + (id + 7)).attr("data-player") !== 0) {
+            if($("#" + (id + 7)).attr("data-player") !== 0){
                 return true;
             }
             return false;
         }
 
     }
-
-    function checkWin(p) {
+    function checkWin(p){
         //Checkar filas
         var filas = 0;
-        for (let i = 0; i < 42; i += 7) {
-            for (let j = 0; j < 7; j++) {
-                let espaco = $("#" + (i + j));
-                if (espaco.attr("data-player") === p) {
+        for(let i = 0; i < 42; i+=7){
+            for(let j = 0; j < 7; j++){
+                let espaco = $("#" +(i + j));
+                if(espaco.attr("data-player") === p) {
                     filas++;
-                } else {
+                }else{
                     filas = 0;
                 }
-                if (filas >= 4) {
+                if(filas >= 4){
                     return true;
                 }
             }
             filas = 0;
         }
+        //checkar colunas
         filas = 0;
-        for (let i = 0; i < 7; i++) {
-            for (let j = 0; j < 42; j += 7) {
-                let espaco = $("#" + (i + j));
-                if (espaco.attr("data-player") === p) {
+        for(let i = 0; i < 7; i++){
+            console.log("Sai")
+            for(let j = 0; j < 42; j+=7){
+                let espaco = $("#" +(i + j));
+                if(espaco.attr("data-player")=== p) {
                     filas++;
-                } else {
+                }else{
                     filas = 0;
                 }
-                if (filas >= 4) {
+                if(filas >= 4){
                     return true;
                 }
             }
-            chain = 0;
+            filas = 0;
         }
+
         return false;
+
     }
+
 });
-
-
-    //checkar colunas
