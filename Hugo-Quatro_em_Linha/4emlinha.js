591a4bb9c941031cc4320e4f630c17b4afc4e3c2
FranciscoLopes01 <cursoupskills@gmail.com>
2022-09-01 12:44:22 +0100

    Gravidade

--- Hugo-Quatro_em_Linha/4emlinha.js
+++ Hugo-Quatro_em_Linha/4emlinha.js
@@ -1,7 +1,9 @@
 let ready = $(document).ready(function(){
     let player= 1;
     let winner = 0;
-    var colors= {};
+    let numberOfLines = 7;
+    let colunas= 6;
+    let colors= {};
     colors[-1]= "yellow";
     colors[1]="red";
     let count = 0;
@@ -13,8 +15,29 @@ let ready = $(document).ready(function(){
 
         $(this).click(function (){
             if(isvalid($(this).attr("id"))){
-                $(this).css("background-color",colors[player]);
-                $(this).attr("data-player",player);
+                const id = Number( $(this).attr("id"))
+                const coluna = id % numberOfLines
+                const linha = parseInt(id / numberOfLines)
+                console.log("linha",linha)
+                console.log("coluna",coluna)
+
+                let lastElementAvailable ;
+                for(let i = 0 ; i < 6; i++){
+                    console.log("index",i)
+                    console.log("coluna",coluna)
+                    console.log("id",getId(i,coluna))
+                    const currentElement = $("#" + getId(i,coluna));
+                    if(currentElement.attr("data-player") === "0"){
+                        lastElementAvailable = currentElement;
+                    }
+                }
+
+
+
+                console.log(linha)
+                console.log(coluna)
+                $(lastElementAvailable).css("background-color",colors[player]);
+                $(lastElementAvailable).attr("data-player",player);
                 if(checkWin(player)){
                     alert(colors[player]+ " Ganhou!")
                     winner = player;
@@ -26,16 +49,29 @@ let ready = $(document).ready(function(){
         });
 
     });
+
+    $("#restart").click(function (){
+        limparjogo();
+    });
+    function getId(linha,coluna){
+        return linha * numberOfLines + coluna;
+
+    }
+    function limparjogo(){
+        $(".Espaço").each(function(){
+            $(this).attr("data-player",0);
+            $(this).css("background-color","white");
+
+            winner= 0
+        })
+    }
     function isvalid(n){
-        var id= parseInt(n);
+        let id= parseInt(n);
         if(winner !== 0){
             return false;
         }
         if($("#"+id).attr("data-player")  === "0"){
-            if(id >= 35) {
-                return true;
-            }
-            if($("#" + (id + 7)).attr("data-player") !== 0){
+            if(id <= 42) {
                 return true;
             }
             return false;
@@ -48,7 +84,7 @@ let ready = $(document).ready(function(){
         for(let i = 0; i < 42; i+=7){
             for(let j = 0; j < 7; j++){
                 let espaco = $("#" +(i + j));
-                if(espaco.attr("data-player") === p) {
+                if(espaco.attr("data-player") == p) {
                     filas++;
                 }else{
                     filas = 0;
@@ -62,10 +98,9 @@ let ready = $(document).ready(function(){
         //checkar colunas
         filas = 0;
         for(let i = 0; i < 7; i++){
-            console.log("Sai")
             for(let j = 0; j < 42; j+=7){
                 let espaco = $("#" +(i + j));
-                if(espaco.attr("data-player")=== p) {
+                if(espaco.attr("data-player")== p) {
                     filas++;
                 }else{
                     filas = 0;
@@ -76,9 +111,34 @@ let ready = $(document).ready(function(){
             }
             filas = 0;
         }
+        //check diagonais
+        let topLeft = 0;
+        let topRight= topLeft + 3;
 
+
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 4; j++){
+                if($("#" + topLeft).attr("data-player") == p
+                    && $("#"+ (topLeft + 8)).attr("data-player") == p
+                    && $("#"+ (topLeft + 16)).attr("data-player") == p
+                    && $("#"+ (topLeft + 24)).attr("data-player") == p) {
+                    return true;
+                }
+                if($("#" + topRight).attr("data-player") == p
+                    && $("#"+ (topRight + 6)).attr("data-player") == p
+                    && $("#"+ (topRight + 12)).attr("data-player") == p
+                    && $("#"+ (topRight + 18)).attr("data-player") == p){
+                    return true;
+                }
+
+                topLeft++;
+                topRight = topLeft + 3;
+
+            }
+            topLeft = i * 7 + 7;
+            topRight = topLeft + 3;
+        }
         return false;
-
     }
 
 });
---
+++ Hugo-Quatro_em_Linha/historico.js
@@ -0,0 +1,8 @@
+let historico [
+    {nome:""}
+    {data:""}
+    {jogo:""}
+    {vencedor:""}
+    {tempo:""}
+]
+ localStorage.setItem("historico_jogos",)
